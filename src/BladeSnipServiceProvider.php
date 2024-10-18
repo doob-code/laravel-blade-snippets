4ad3c26a0a21488f20e68ef2da842fbb53a39729
Pete <pete@pete-debian.lan>
2024-10-18 14:55:58 +0100

    Initial commit

---
+++ src/BladeSnipServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DoobCode\BladeSnippets;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+
+class BladeSnippetServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Define the custom Blade directive @snip
+        Blade::directive('snip', function ($variableName) {
+            return "<?php ob_start(); ?>";
+        });
+
+        // Define the custom Blade directive @endsnip
+        Blade::directive('endsnip', function ($variableName) {
+            return "<?php 
+                \${$variableName} = Blade::render(ob_get_clean()); 
+            ?>";
+        });
+    }
+
+}
+
